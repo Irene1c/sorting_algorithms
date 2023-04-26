3188d84bc0804422a24ed2939bbf54a0613b0982
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-04-26 06:56:33 -0700

    update header file

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int divide(int *array, size_t size, int first, int last);
+void sort(int *array, size_t size, int first, int last);
 
 #endif
